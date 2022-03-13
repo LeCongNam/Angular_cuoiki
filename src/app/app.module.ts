3d7664aa23e5bb968ae0e592f4e58231a7484cb2
LeCongNam <namlem4u@gmail.com>
2022-03-13 21:11:47 +0700

    fix conflix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {  HttpClientModule } from '@angular/common/http';
@@ -44,50 +43,3 @@ import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-=======
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import {  HttpClientModule } from '@angular/common/http';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { MenuComponent } from './menu/menu.component';
-import { ContentComponent } from './content/content.component';
-import { LoggerService } from './services/logger.service';
-import { FooterComponent } from './footer/footer.component';
-import { DatePipe } from './pipe/date.pipe';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { AddFormComponent } from './add-form/add-form.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoaddingComponent } from './content/loadding/loadding.component';
-import { UpdateItemComponent } from './content/update-item/update-item.component';
-import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    SidebarComponent,
-    MenuComponent,
-    ContentComponent,
-    FooterComponent,
-    DatePipe,
-    AddFormComponent,
-    LoaddingComponent,
-    UpdateItemComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FontAwesomeModule,
-    FormsModule,
-    ReactiveFormsModule,
-    SweetAlert2Module,
-    SweetAlert2Module.forRoot(),
-  ],
-  providers: [LoggerService],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
->>>>>>> 0e543a37e6ecfbcb12cbb98e5844e83a7d378ab4
--- src/app/services/projectServices.ts
+++ src/app/services/projectServices.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { HttpClient } from '@angular/common/http';
 import { Injectable,Injector } from '@angular/core';
 import { BaseService } from './base.service';
@@ -30,37 +29,4 @@ export class ProjectServices extends BaseService{
     deleteProject(params:any){
        return this.delete('/api/delete/'+params)
     }
-=======
-import { HttpClient } from '@angular/common/http';
-import { Injectable,Injector } from '@angular/core';
-import { BaseService } from './base.service';
-
-@Injectable({
-  providedIn: 'root'
-})
-
-export class ProjectServices extends BaseService{
-   
-    constructor(private inject:Injector){
-        super(inject)
-    }
-
-    getProject(params:any){
-        return this.get('/api/home',params)
-    }
-
-    addProject(body:any){
-        return this.post('/api/create',body)
-    }
-
-
-    updateProject(id:any,body:any){
-        return this.patch('/api/update/'+id,body)
-    }
-   
-
-    deleteProject(params:any){
-       return this.delete('/api/delete/'+params)
-    }
->>>>>>> 0e543a37e6ecfbcb12cbb98e5844e83a7d378ab4
 }
